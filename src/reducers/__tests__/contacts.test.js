2f20b154308b882e8bc1d3457d63604435dc64ac
nathanhannig <breakpoint@gmail.com>
2018-07-17 15:56:23 -0700

    Refactored reducer tests to utilize deep-freeze on parameters

--- src/reducers/__tests__/contacts.test.js
+++ src/reducers/__tests__/contacts.test.js
@@ -1,116 +1,115 @@
+import deepFreeze from 'deep-freeze';
 import reducer from '../contacts';
 import { CONTACT_CREATE, CONTACT_UPDATE, CONTACT_DELETE } from '../../actions/types';
 
-it('handles actions of type CONTACT_CREATE', () => {
-  const contact = {
-    '123-4567-890': {
-      name: 'Test User 1',
-      phone: '133-152-9222',
-      address: '46090 Ridge Oak Drive',
-      nickname: 'User 1',
-    },
-  };
+describe('contact reducer', () => {
+  it('returns the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({});
+  });
 
-  const initialState = {};
+  it('handles actions of type CONTACT_CREATE', () => {
+    const contact = {
+      '123-4567-890': {
+        name: 'Test User 1',
+        phone: '133-152-9222',
+        address: '46090 Ridge Oak Drive',
+        nickname: 'User 1',
+      },
+    };
 
-  const action = {
-    type: CONTACT_CREATE,
-    payload: contact,
-  };
+    const initialState = {};
 
-  const newState = reducer(initialState, action);
+    const action = {
+      type: CONTACT_CREATE,
+      payload: contact,
+    };
 
-  // Check that reducer is pure function and
-  // did not mutate parameters
-  expect(action).toBe(action);
-  expect(contact).toBe(contact);
+    // Check that reducer is pure function and
+    // did not mutate parameters
+    deepFreeze(initialState);
+    deepFreeze(action);
 
-  // Check that state has changed
-  expect(newState).not.toBe(initialState);
+    const newState = reducer(initialState, action);
 
-  // Check the state has the correct data
-  expect(newState).toEqual(contact);
-});
+    // Check the state has the correct data
+    expect(newState).toEqual(contact);
+  });
 
-it('handles actions of type CONTACT_UPDATE', () => {
-  const contact = {
-    '123-4567-890': {
-      name: 'Test User 1 Updated',
-      phone: '133-152-9222',
-      address: '46090 Ridge Oak Drive',
-      nickname: 'User 1',
-    },
-  };
+  it('handles actions of type CONTACT_UPDATE', () => {
+    const contact = {
+      '123-4567-890': {
+        name: 'Test User 1 Updated',
+        phone: '133-152-9222',
+        address: '46090 Ridge Oak Drive',
+        nickname: 'User 1',
+      },
+    };
 
-  const initialState = {
-    '123-4567-890': {
-      name: 'Test User 1',
-      phone: '133-152-9222',
-      address: '46090 Ridge Oak Drive',
-      nickname: 'User 1',
-    },
-  };
+    const initialState = {
+      '123-4567-890': {
+        name: 'Test User 1',
+        phone: '133-152-9222',
+        address: '46090 Ridge Oak Drive',
+        nickname: 'User 1',
+      },
+    };
 
-  const action = {
-    type: CONTACT_UPDATE,
-    payload: contact,
-  };
+    const action = {
+      type: CONTACT_UPDATE,
+      payload: contact,
+    };
 
-  const newState = reducer(initialState, action);
+    // Check that reducer is pure function and
+    // did not mutate parameters
+    deepFreeze(initialState);
+    deepFreeze(action);
 
-  // Check that reducer is pure function and
-  // did not mutate parameters
-  expect(action).toBe(action);
-  expect(contact).toBe(contact);
+    const newState = reducer(initialState, action);
 
-  // Check that state has changed
-  expect(newState).not.toBe(initialState);
+    // Check the state has the correct data
+    expect(newState).toEqual(contact);
+  });
 
-  // Check the state has the correct data
-  expect(newState).toEqual(contact);
-});
+  it('handles actions of type CONTACT_DELETE', () => {
+    const contactId = '123-4567-890';
 
-it('handles actions of type CONTACT_DELETE', () => {
-  const contactId = '123-4567-890';
+    const initialState = {
+      '123-4567-890': {
+        name: 'Test User 1',
+        phone: '133-152-9222',
+        address: '46090 Ridge Oak Drive',
+        nickname: 'User 1',
+      },
+    };
 
-  const initialState = {
-    '123-4567-890': {
-      name: 'Test User 1',
-      phone: '133-152-9222',
-      address: '46090 Ridge Oak Drive',
-      nickname: 'User 1',
-    },
-  };
+    const action = {
+      type: CONTACT_DELETE,
+      payload: contactId,
+    };
 
-  const action = {
-    type: CONTACT_DELETE,
-    payload: contactId,
-  };
+    // Check that reducer is pure function and
+    // did not mutate parameters
+    deepFreeze(initialState);
+    deepFreeze(action);
 
-  const newState = reducer(initialState, action);
+    const newState = reducer(initialState, action);
 
-  // Check that reducer is pure function and
-  // did not mutate parameters
-  expect(action).toBe(action);
-  expect(contactId).toBe(contactId);
+    // Check the state has the correct data
+    expect(newState).toEqual({});
+  });
 
-  // Check that state has changed
-  expect(newState).not.toBe(initialState);
+  it('handles action with unknown type', () => {
+    const initialState = {};
+    const action = {};
 
-  // Check the state has the correct data
-  expect(newState).toEqual({});
-});
+    // Check that reducer is pure function and
+    // did not mutate parameters
+    deepFreeze(initialState);
+    deepFreeze(action);
 
-it('handles action with unknown type', () => {
-  const initialState = {};
-  const action = {};
+    const newState = reducer(initialState, action);
 
-  const newState = reducer(initialState, action);
-
-  // Check that reducer is pure function and
-  // did not mutate parameters
-  expect(action).toBe(action);
-
-  // Check that state hasn't changed
-  expect(newState).toBe(initialState);
+    // Check that state hasn't changed
+    expect(newState).toBe(initialState);
+  });
 });
